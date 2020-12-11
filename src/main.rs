df0db28ee88f19cd364e60abcf53d24739d00bbb
ian <me@iany.me>
2020-12-11 17:03:15 +0800

    Start a tentacle node

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use tentacle::{
+    builder::ServiceBuilder, context::ServiceContext, secio::SecioKeyPair, service::ServiceEvent,
+    traits::ServiceHandle,
+};
+
+struct AppServiceHandle;
+
+impl ServiceHandle for AppServiceHandle {
+    fn handle_event(&mut self, _control: &mut ServiceContext, event: ServiceEvent) {
+        if let ServiceEvent::ListenStarted { address: _ } = event {
+            log::info!("Hello, Tentacle");
+        }
+
+        log::info!("handle_event: {:?}", event);
+    }
+}
+
 fn main() {
     {
         use log::LevelFilter::Info;
@@ -7,6 +24,20 @@ fn main() {
     let mut rt = tokio::runtime::Runtime::new().expect("create tokio runtime");
 
     rt.block_on(async {
-        log::info!("Hello, Tokio");
+        let mut app_service = ServiceBuilder::default()
+            .key_pair(SecioKeyPair::secp256k1_generated())
+            .build(AppServiceHandle);
+
+        app_service
+            .listen("/ip4/127.0.0.1/tcp/1234".parse().unwrap())
+            .await
+            .expect("listen on 127.0.0.1:1234");
+
+        {
+            use futures::stream::StreamExt;
+            while app_service.next().await.is_some() {
+                // loop
+            }
+        }
     });
 }
