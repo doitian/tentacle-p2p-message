6c5fe9368fe418fec302228ff65beeb1a41caa89
ian <me@iany.me>
2020-12-14 14:03:25 +0800

    Parse command line arguments

--- src/main.rs
+++ src/main.rs
@@ -15,23 +15,81 @@ impl ServiceHandle for AppServiceHandle {
     }
 }
 
+struct AppArgs {
+    port: u16,
+    bootnode: Option<String>,
+    target_peer_id: Option<String>,
+    message: Option<String>,
+}
+
+impl Default for AppArgs {
+    fn default() -> Self {
+        Self {
+            port: 1234,
+            bootnode: None,
+            target_peer_id: None,
+            message: None,
+        }
+    }
+}
+
+/// Parses the command line args.
+///
+/// ## Usage
+///
+/// * `p2p-message`: start a node listening on the default port 1234.
+/// * `p2p-message port`: start a node listening on the specified port.
+/// * `p2p-message port bootnode`: start a node listening on the specified port and connect to
+/// another node as the bootnode.
+/// * `p2p-message port bootnode target_peer_id message`: start a node, connect to the bootnode, then send a message to `target_peer_id`.
+fn parse_args() -> AppArgs {
+    let mut parsed_args = AppArgs::default();
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() > 1 {
+        {
+            use std::str::FromStr;
+            parsed_args.port = u16::from_str(&args[1]).expect("port number");
+        }
+    }
+    if args.len() > 2 && !args[2].is_empty() {
+        parsed_args.bootnode = Some(args[2].clone());
+    }
+    if args.len() > 3 && !args[3].is_empty() {
+        parsed_args.target_peer_id = Some(args[3].clone());
+    }
+    if args.len() > 4 && !args[4].is_empty() {
+        parsed_args.message = Some(args[4].clone());
+    }
+
+    parsed_args
+}
+
 fn main() {
     {
         use log::LevelFilter::Info;
         env_logger::builder().filter_level(Info).init();
     }
 
+    let args = parse_args();
+
     let mut rt = tokio::runtime::Runtime::new().expect("create tokio runtime");
 
     rt.block_on(async {
+        let key_pair = SecioKeyPair::secp256k1_generated();
+        log::info!(
+            "listen on /ip4/127.0.0.1/tcp/{}/p2p/{}",
+            args.port,
+            key_pair.peer_id().to_base58()
+        );
+
         let mut app_service = ServiceBuilder::default()
-            .key_pair(SecioKeyPair::secp256k1_generated())
+            .key_pair(key_pair)
             .build(AppServiceHandle);
 
         app_service
-            .listen("/ip4/127.0.0.1/tcp/1234".parse().unwrap())
+            .listen(format!("/ip4/127.0.0.1/tcp/{}", args.port).parse().unwrap())
             .await
-            .expect("listen on 127.0.0.1:1234");
+            .expect("listen");
 
         {
             use futures::stream::StreamExt;
